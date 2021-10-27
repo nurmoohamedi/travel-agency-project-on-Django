c8de21dd8ea01cfc0e23e63bc38d91a92a78fa0c
nurmoohamedi <nurmoohamedi@gmail.com>
2021-10-27 16:58:26 +0600

    Added Dictionary into contact.html page, about member informations

--- web_tech_django/apps/blog/views.py
+++ web_tech_django/apps/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-team_members = {
+team_members = ({
   "fname": "Muhamedi",
   "lname": "Nurmuhammed",
   "bdate": 2002,
@@ -8,16 +8,16 @@ team_members = {
   "phone": "+77471970242",
   "group": "ITSE-1902",
   "position": "Director/Developer"
-}
+})
 
-# Create your views here.
+
 def index(request):
     return render(request, 'blog/index.html')
 
 
-def about(request):
-    return render(request, 'blog/about.html', team_members)
-
-
 def contact(request):
-    return render(request, 'blog/contact.html', team_members)
+    return render(request, 'blog/contact.html', {'team': team_members})
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'team': team_members})
