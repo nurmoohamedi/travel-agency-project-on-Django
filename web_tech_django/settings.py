a2918d531dec7c3ca01a6714bd6fe41d875c51fa
nurmoohamedi <nurmoohamedi@gmail.com>
2021-11-11 14:40:07 +0600

    added allowed host

--- web_tech_django/settings.py
+++ web_tech_django/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-#8e#4u_^x*(r3fjsmmk^_^7xk=gdec(2i@oy4@i3&f(fp=-0z7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nurmoohamedi.pythonanywhere.com']
 
 
 # Application definition
