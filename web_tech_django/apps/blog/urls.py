7b0b661fbd9dbe134636fc026faf8e0ae60a1a29
nurmoohamedi <nurmoohamedi@gmail.com>
2021-11-18 05:27:58 +0600

    added two templates, urls, views

--- web_tech_django/apps/blog/urls.py
+++ web_tech_django/apps/blog/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
+    path('tours', views.tours, name='tours'),
+    path('services', views.services, name='services'),
     path('admin', admin.site.urls),
 ]
--- web_tech_django/apps/blog/views.py
+++ web_tech_django/apps/blog/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from .models import Member
 
-
 team_members = ({
-  "fname": "Muhamedi",
-  "lname": "Nurmuhammed",
-  "bdate": 2002,
-  "email": "nurmoohamedi@gmail.com",
-  "phone": "+77471970242",
-  "group": "ITSE-1902",
-  "position": "Director/Developer"
+    "fname": "Muhamedi",
+    "lname": "Nurmuhammed",
+    "bdate": 2002,
+    "email": "nurmoohamedi@gmail.com",
+    "phone": "+77471970242",
+    "group": "ITSE-1902",
+    "position": "Director/Developer"
 })
 
 
@@ -18,7 +17,6 @@ def index(request):
 
 
 def contact(request):
-
     members = Member.objects.all()
 
     return render(request, 'blog/contact.html', {'team': team_members, 'members': members})
@@ -26,3 +24,11 @@ def contact(request):
 
 def about(request):
     return render(request, 'blog/about.html', {'team': team_members})
+
+
+def tours(request):
+    return render(request, 'blog/tours.html')
+
+
+def services(request):
+    return render(request, 'blog/services.html')
