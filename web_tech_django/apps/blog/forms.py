c1333f0063ba3beeeb9b56b78ad214e91a365dcf
nurmoohamedi <nurmoohamedi@gmail.com>
2021-12-23 14:53:15 +0600

    added Hotel model, OrderHotel, ordering operations

--- web_tech_django/apps/blog/forms.py
+++ web_tech_django/apps/blog/forms.py
@@ -1,11 +1,11 @@
-from .models import Tour
+from .models import Tour, Customer, Hotel
 from django.forms import ModelForm, TextInput, Textarea, NumberInput, DateInput
 
 
 class TourForm(ModelForm):
     class Meta:
         model = Tour
-        fields = ("name", "description", "started", "price", "duration")
+        fields = "__all__"
         widgets = {
             "name": TextInput(attrs={
                 'class': 'form-control', 'placeholder': 'Enter the name'
@@ -22,4 +22,34 @@ class TourForm(ModelForm):
             "duration": NumberInput(attrs={
                 'class': 'form-control', 'placeholder': 'Enter duration'
             }),
+        }
+
+
+class HotelForm(ModelForm):
+    class Meta:
+        model = Hotel
+        fields = "__all__"
+        # widgets = {
+        #     "name": TextInput(attrs={
+        #         'class': 'form-control', 'placeholder': 'Enter the name of Hotel'
+        #     }),
+        #     "city": Textarea(attrs={
+        #         'class': 'form-control', 'placeholder': 'Enter the city'
+        #     }),
+        #     "hotelClass": DateInput(attrs={
+        #         'class': 'form-control', 'placeholder': 'Enter the class of a hotel'
+        #     }),
+        #     "price": NumberInput(attrs={
+        #         'class': 'form-control', 'placeholder': 'Enter price of the hotel to the one person'
+        #     }),
+        # }
+
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = "__all__"
+        widgets = {
+            "phone": TextInput(attrs={'class':'form-control'}),
+            "place": TextInput(attrs={'class': 'form-control'}),
         }
--- web_tech_django/apps/blog/models.py
+++ web_tech_django/apps/blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -12,14 +13,58 @@ class Member(models.Model):
         return self.name
 
 
+class Customer(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=12)
+    place = models.CharField(max_length=50)
+
+
 class Tour(models.Model):
-    id = models.IntegerField('id', primary_key=True)
-    name = models.CharField('tour_name', max_length=60)
-    description = models.TextField('tour_name')
-    started = models.DateTimeField('started date')
-    duration = models.IntegerField('tour_duration')
-    price = models.IntegerField('tour price')
-    image = models.FileField(upload_to='tours/')
+    name = models.CharField('Tour name', max_length=60)
+    description = models.TextField('Tour description')
+    started = models.DateTimeField('Started date')
+    duration = models.IntegerField('Tour duration')
+    price = models.IntegerField('Tour price')
+    image = models.ImageField('Tour image', upload_to='tours/')
+    # customer = models.ManyToManyField(Customer)
 
     def __str__(self):
         return self.name
+
+
+class OrderTour(models.Model):
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    # orders =
+
+    def __str__(self):
+        return self.tour.name
+
+
+class Hotel(models.Model):
+    name = models.CharField('Hotel name', max_length=60)
+    city = models.CharField('Hotel city', max_length=60)
+    price = models.IntegerField('Price for one person')
+    hotelClass = models.IntegerField('Class of the hotel')
+    image = models.ImageField('Tour image', upload_to='hotels/')
+
+    def __str__(self):
+        return self.name
+
+
+class OrderHotel(models.Model):
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.hotel.name
+
+
+# class Order(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     start_date = models.DateTimeField(auto_now_add=True)
+#     ordered = models.BooleanField(default=False)
+#
+#     def __str__(self):
+#         return self.user.name
